99900e57dd4618f82b224a33850cdba6c99f534d
Andrew Gasser <agasser01@hotmail.com>
2023-05-25 14:29:02 -0500

    Remove Orion controls

--- src/main/java/frc/robot/controls/ControlBindings.java
+++ src/main/java/frc/robot/controls/ControlBindings.java
@@ -3,36 +3,15 @@ package frc.robot.controls;
 import java.util.Optional;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public interface ControlBindings {
   Optional<Trigger> resetPose();
   Optional<Trigger> reseedSteerMotors();
-  Optional<Trigger> fieldHeadingDrive();
-  Optional<Trigger> fieldOrientedDrive();
   Optional<Trigger> wheelsToX();
-  Optional<Trigger> elevatorUp();
-  Optional<Trigger> elevatorDown();
-  Optional<Trigger> wristUp();
-  Optional<Trigger> wristDown();
-  Optional<Trigger> shooterIn();
-  Optional<Trigger> shooterOut();
-  Optional<Trigger> autoIntake();
-  Optional<Trigger> doubleStationPickup();
-  Optional<Trigger> manualCube();
-  Optional<Trigger> manualCone();
-  Optional<Trigger> shootAutomatically();
-  Optional<Trigger> shootHigh();
-  Optional<Trigger> shootMid();
-  Optional<Trigger> shootLow();
-  Optional<Trigger> launchCube();
-  Optional<Trigger> driveSingleSubstation();
   DoubleSupplier translationX();
   DoubleSupplier translationY();
   DoubleSupplier omega();
-  Supplier<Optional<Rotation2d>> heading();
   BooleanSupplier driverWantsControl();
 }
--- src/main/java/frc/robot/controls/XBoxControlBindings.java
+++ src/main/java/frc/robot/controls/XBoxControlBindings.java
@@ -7,57 +7,14 @@ import static frc.robot.Constants.TeleopDriveConstants.XBOX_CONTROLLER_DEADBAND;
 import java.util.Optional;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.DrivetrainConstants;
 
 public class XBoxControlBindings implements ControlBindings {
 
   private final CommandXboxController driverController = new CommandXboxController(0);;
-
-  @Override
-  public Optional<Trigger> elevatorDown() {
-    return Optional.empty();
-  }
-
-  @Override
-  public Optional<Trigger> elevatorUp() {
-    return Optional.empty();
-  }
-
-  @Override
-  public Optional<Trigger> fieldHeadingDrive() {
-    return Optional.empty();
-  }
-
-  @Override
-  public Optional<Trigger> fieldOrientedDrive() {
-    return Optional.empty();
-  }
-
-  @Override
-  public Optional<Trigger> autoIntake() {
-    return Optional.of(driverController.leftTrigger());
-  }
-
-  @Override
-  public Optional<Trigger> manualCube() {
-    return Optional.empty();
-  }
-
-  @Override
-  public Optional<Trigger> manualCone() {
-    return Optional.empty();
-  }
-
-  @Override
-  public Optional<Trigger> doubleStationPickup() {
-    return Optional.empty();
-  }
 
   @Override
   public Optional<Trigger> reseedSteerMotors() {
@@ -70,60 +27,10 @@ public class XBoxControlBindings implements ControlBindings {
   }
 
   @Override
-  public Optional<Trigger> shootAutomatically() {
-    return Optional.of(driverController.b());
-  }
-  
-  @Override
-  public Optional<Trigger> shootHigh() {
-    return Optional.of(driverController.y());
-  }
-
-  @Override
-  public Optional<Trigger> shootLow() {
-    return Optional.of(driverController.a());
-  }
-
-  @Override
-  public Optional<Trigger> shootMid() {
-    return Optional.of(driverController.x());
-  }
-
-  @Override
-  public Optional<Trigger> shooterIn() {
-    return Optional.of(driverController.leftBumper());
-  }
-
-  @Override
-  public Optional<Trigger> shooterOut() {
-    return Optional.of(driverController.rightBumper());
-  }
-
-  @Override
   public Optional<Trigger> wheelsToX() {
     return Optional.of(driverController.povRight());
   }
 
-  @Override
-  public Optional<Trigger> wristDown() {
-    return Optional.empty();
-  }
-
-  @Override
-  public Optional<Trigger> wristUp() {
-    return Optional.empty();
-  }
-
-  @Override
-  public Optional<Trigger> launchCube() {
-    return Optional.of(driverController.rightTrigger());
-  }
-
-  @Override
-  public Optional<Trigger> driveSingleSubstation() {
-    return Optional.empty();
-  }
-  
   @Override
   public DoubleSupplier translationX() {
     return () ->-modifyAxis(driverController.getLeftY()) * MAX_VELOCITY_METERS_PER_SECOND;
@@ -136,23 +43,6 @@ public class XBoxControlBindings implements ControlBindings {
   @Override
   public DoubleSupplier omega() {
     return () -> -modifyAxis(driverController.getRightX()) * MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND / 2;
-  }
-
-  @Override
-  public Supplier<Optional<Rotation2d>> heading() {
-    return () -> {
-      final var thetaX = -modifyAxis(driverController.getLeftY()) * DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND;
-      final var thetaY = -modifyAxis(driverController.getLeftX()) * DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND;
-      final var centered = MathUtil.applyDeadband(thetaX, XBOX_CONTROLLER_DEADBAND) == 0
-          && MathUtil.applyDeadband(thetaY, XBOX_CONTROLLER_DEADBAND) == 0;
-  
-      if (centered) {
-        // Hold heading when stick is centered
-        return Optional.empty();
-      }
-        // Calculate heading from Y-Axis to X, Y coordinates
-        return Optional.of(new Rotation2d(thetaX, thetaY));
-    };
   }
 
   @Override
