cc2e50444bd7ff84488421f21812d423471ab31e
Andrew Gasser <agasser01@hotmail.com>
2023-05-25 14:54:01 -0500

    Remove elevator offset and other clean up

--- src/main/java/frc/robot/limelight/LimelightCalcs.java
+++ src/main/java/frc/robot/limelight/LimelightCalcs.java
@@ -1,7 +1,5 @@
 package frc.robot.limelight;
 
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
@@ -18,57 +16,18 @@ public class LimelightCalcs {
   private final Transform3d cameraToRobot;
   private final Transform2d robotToCamera2d;
   private final double targetHeight;
-  private final DoubleSupplier cameraHeightOffsetSupplier;
 
   /**
    * Constructor
    * @param cameraToRobot transform from the camera to the robot
    * @param targetHeight height of the target
-   * @param cameraHeightOffsetSupplier supplier for an offset to add to the camera height. Useful for cameras
-   *            mounted on things that move up and down, like elevators.
    */
-  public LimelightCalcs(Transform3d cameraToRobot, double targetHeight, DoubleSupplier cameraHeightOffsetSupplier) {
+  public LimelightCalcs(Transform3d cameraToRobot, double targetHeight) {
     this.cameraToRobot = cameraToRobot;
     this.robotToCamera2d = new Transform2d(
         cameraToRobot.getTranslation().toTranslation2d(),
         cameraToRobot.getRotation().toRotation2d()).inverse();
     this.targetHeight = targetHeight;
-    this.cameraHeightOffsetSupplier = cameraHeightOffsetSupplier;
-  }
-
-  /**
-   * Constructor
-   * @param cameraToRobot transform from the camera to the robot
-   * @param targetHeight height of the target
-   */
-  public LimelightCalcs(Transform3d cameraToRobot, double targetHeigt) {
-    this(cameraToRobot, targetHeigt, () -> 0);
-  }
-
-  /**
-   * Get distance from camera to target, along the floor
-   * @param targetYDegrees Y coordinate of the target in degrees
-   * @return distance from the camera to the target
-   */
-  protected double getCameraToTargetDistance(double targetYDegrees) {
-    var cameraPitch = -cameraToRobot.getRotation().getY();
-    var cameraHeight = -cameraToRobot.getZ() + cameraHeightOffsetSupplier.getAsDouble();
-    return (targetHeight - cameraHeight)
-        / Math.tan(cameraPitch + Units.degreesToRadians(targetYDegrees));
-  }
-
-  /**
-   * Gets the robot relative translation of the target
-   * @param targetXDegrees X coordinate of the target in degrees
-   * @param targetYDegrees Y coordinate of the target in degrees
-   * @return robot relative translaction
-   */
-  public Translation2d getTargetTranslation(double targetXDegrees, double targetYDegrees) {
-    var targetOnCameraCoordinates = new Translation2d(
-        getCameraToTargetDistance(targetYDegrees),
-        Rotation2d.fromDegrees(-targetXDegrees));
-
-    return targetOnCameraCoordinates.plus(robotToCamera2d.getTranslation().rotateBy(robotToCamera2d.getRotation()));
   }
 
   /**
@@ -102,6 +61,32 @@ public class LimelightCalcs {
    */
   public VisionTargetInfo getRobotRelativeTargetInfo(LimelightDetectorTarget detectorTarget) {
     return getRobotRelativeTargetInfo(detectorTarget.targetXDegrees, detectorTarget.targetYDegrees);
+  }
+
+  /**
+   * Get distance from camera to target, along the floor
+   * @param targetYDegrees Y coordinate of the target in degrees
+   * @return distance from the camera to the target
+   */
+  protected double getCameraToTargetDistance(double targetYDegrees) {
+    var cameraPitch = -cameraToRobot.getRotation().getY();
+    var cameraHeight = -cameraToRobot.getZ();
+    return (targetHeight - cameraHeight)
+        / Math.tan(cameraPitch + Units.degreesToRadians(targetYDegrees));
+  }
+
+  /**
+   * Gets the robot relative translation of the target
+   * @param targetXDegrees X coordinate of the target in degrees
+   * @param targetYDegrees Y coordinate of the target in degrees
+   * @return robot relative translaction
+   */
+  protected Translation2d getTargetTranslation(double targetXDegrees, double targetYDegrees) {
+    var targetOnCameraCoordinates = new Translation2d(
+        getCameraToTargetDistance(targetYDegrees),
+        Rotation2d.fromDegrees(-targetXDegrees));
+
+    return targetOnCameraCoordinates.plus(robotToCamera2d.getTranslation().rotateBy(robotToCamera2d.getRotation()));
   }
 
 }
