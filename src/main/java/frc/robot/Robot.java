7aa78e06d7d920c511b4f7a0f60274a65ac8a5bd
Andrew Gasser <agasser01@hotmail.com>
2023-06-06 16:18:00 -0500

    Suppress stupid AdvantageKit messages

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -130,6 +130,16 @@ public class Robot extends LoggedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
+  
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {
+  }
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {
+  }
 
   /**
    * Checks the driverstation alliance. We have have to check repeatedly because we don't know when the
