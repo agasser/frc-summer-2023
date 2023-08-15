04854cb72b4b9e327b7ec109cc0beac3078cc47e
Andrew Gasser <agasser01@hotmail.com>
2023-08-15 09:45:37 -0500

    Method chaining

--- src/main/java/frc/robot/swerve/SwerveSteerController.java
+++ src/main/java/frc/robot/swerve/SwerveSteerController.java
@@ -117,8 +117,7 @@ public class SwerveSteerController {
    * @param desiredRotation desired rotation setpoint
    */
   public void setDesiredRotation(Rotation2d desiredRotation) {
-    motionMagicVoltageRequest.Position = desiredRotation.getRotations();
-    motor.setControl(motionMagicVoltageRequest);
+    motor.setControl(motionMagicVoltageRequest.withPosition(desiredRotation.getRotations()));
   }
 
   /**
