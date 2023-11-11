1b0f30425f1080a3012b82956cc4458d58f8e857
Andrew Gasser <agasser01@hotmail.com>
2023-11-11 12:27:46 -0600

    Library updates

--- src/main/java/frc/robot/AutonomousBuilder.java
+++ src/main/java/frc/robot/AutonomousBuilder.java
@@ -1,34 +1,25 @@
 package frc.robot;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-import java.util.stream.Stream;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants.AutoDriveConstants;
 import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class AutonomousBuilder {
 
-  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+  private final SendableChooser<Command> autoChooser;
 
   public AutonomousBuilder(DrivetrainSubsystem drivetrainSubsystem, Supplier<Pose2d> poseSupplier,
       Consumer<Pose2d> poseConsumer) {
@@ -48,20 +39,7 @@ public class AutonomousBuilder {
             new ReplanningConfig()),
         drivetrainSubsystem);
 
-    autoChooser.setDefaultOption("None", Commands.none());
-    // Add all the autos in the pathplanner directory
-    try (Stream<Path> files = Files
-        .list(Paths.get(Filesystem.getDeployDirectory().getAbsolutePath(), "pathplanner/autos"))) {
-      files.filter(file -> !Files.isDirectory(file))
-          .map(Path::getFileName)
-          .map(Path::toString)
-          .filter(fileName -> fileName.endsWith(".auto"))
-          .sorted()
-          .map(pathName -> pathName.substring(0, pathName.lastIndexOf(".")))
-          .forEach(pathName -> autoChooser.addOption("PP: " + pathName, new PathPlannerAuto(pathName)));
-    } catch (IOException e) {
-      DriverStation.reportError("********* Failed to list PathPlanner paths. *********", false);
-    }
+    autoChooser = AutoBuilder.buildAutoChooser();
   }
 
   /**
